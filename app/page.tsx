ac7c14599c476419c76448a92650c5240df07463
Vivek Maurya <Vivek563maurya@gmail.com>
2024-06-24 08:32:05 +0530

    feat: added shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
 		<>
 			<h1>vivedsfk</h1>
+			<Button>Hello</Button>
 		</>
 	);
 }
