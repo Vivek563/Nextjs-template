63060359bc19859d8cefe2caa404ef91236ee0fb
Vivek Maurya <Vivek563maurya@gmail.com>
2024-06-24 08:29:55 +0530

    feat: added pre installed nextjs templete

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+	return (
+		<>
+			<h1>vivedsfk</h1>
+			<Button>Vivek</Button>
+		</>
+	);
+}
